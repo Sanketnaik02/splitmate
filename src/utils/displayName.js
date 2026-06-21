f4d48b01c3c6145c1112c3e92fe2d99326c14b67
Sanketnaik02 <sanketnaik393@gmail.com>
2026-06-21 19:42:12 +0530

    epenses changes

--- src/utils/displayName.js
+++ src/utils/displayName.js
@@ -2,6 +2,7 @@ import { store } from './storage';
 
 export function getDisplayName(uid, members = []) {
   const member = members.find((m) => String(m.userId) === String(uid));
+  console.log('[getDisplayName] lookup:', { uid, uidType: typeof uid, uidLength: uid?.length, memberFound: !!member, memberUserId: member?.userId, memberUserIdType: typeof member?.userId, memberId: member?.id, memberDisplayName: member?.displayName, allMemberUserIds: members.map(m => ({ userId: m.userId, userIdType: typeof m.userId, id: m.id, displayName: m.displayName })) });
   if (member?.displayName) return member.displayName;
 
   const user = store.get('users', uid);
